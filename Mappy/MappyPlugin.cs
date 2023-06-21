9cbfc0ce0c224711847dc43e3ccd9673d00cfc17
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-06-21 11:14:12 -0700

    Add Unversioned Files

---
+++ Mappy/MappyPlugin.cs
@@ -0,0 +1,36 @@
+﻿using DailyDuty.System;
+using DailyDuty.Views.Windows;
+using Dalamud.Plugin;
+using KamiLib;
+using KamiLib.Commands;
+using Mappy.System.Localization;
+
+namespace DailyDuty;
+
+public sealed class MappyPlugin : IDalamudPlugin
+{
+    public string Name => "Mappy";
+
+    public static MappySystem System = null!;
+    
+    public MappyPlugin(DalamudPluginInterface pluginInterface)
+    {
+        pluginInterface.Create<Service>();
+
+        KamiCommon.Initialize(pluginInterface, Name);
+        KamiCommon.RegisterLocalizationHandler(key => Strings.ResourceManager.GetString(key, Strings.Culture));
+                
+        System = new MappySystem();
+
+        CommandController.RegisterMainCommand("/mappy");
+        
+        KamiCommon.WindowManager.AddConfigurationWindow(new ConfigurationWindow());
+    }
+
+    public void Dispose()
+    {
+        KamiCommon.Dispose();
+        
+        System.Dispose();
+    }
+}
---
+++ Mappy/Service.cs
@@ -0,0 +1,23 @@
+﻿using Dalamud.Game;
+using Dalamud.Game.ClientState;
+using Dalamud.Game.ClientState.Objects;
+using Dalamud.Game.DutyState;
+using Dalamud.Game.Gui;
+using Dalamud.Game.Gui.Toast;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+
+namespace DailyDuty;
+
+public sealed class Service
+{
+    [PluginService] public static DalamudPluginInterface PluginInterface { get; set; } = null!;
+    [PluginService] public static ChatGui Chat { get; set; } = null!;
+    [PluginService] public static ClientState ClientState { get; set; } = null!;
+    [PluginService] public static Framework Framework { get; set; } = null!;
+    [PluginService] public static GameGui GameGui { get; set; } = null!;
+    [PluginService] public static TargetManager TargetManager { get; set; } = null!;
+    [PluginService] public static DutyState DutyState { get; set; } = null!;
+    [PluginService] public static ToastGui Toast { get; set; } = null!;
+    [PluginService] public static ObjectTable ObjectTable { get; set; } = null!;
+}
