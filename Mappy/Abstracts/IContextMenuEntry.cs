cef4c3560d7e96130cbb09f94b2c167485dc1f1e
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-07-11 12:00:10 -0700

    Add Context Menu interface

---
+++ Mappy/Abstracts/IContextMenuEntry.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+using Mappy.System;
+
+namespace Mappy.Abstracts;
+
+public interface IContextMenuEntry
+{
+    ContextMenuType[] MenuTypes { get; }
+    
+    bool Visible { get; }
+    
+    string Label { get; }
+
+    void ClickAction(Vector2 clickPosition);
+}
