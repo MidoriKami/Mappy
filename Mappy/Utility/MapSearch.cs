2176af2364f14f190fb74603cbebdc2ec7251900
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-07-14 13:33:26 -0700

    Probably fix search results
    closes #20

--- Mappy/Utility/MapSearch.cs
+++ Mappy/Utility/MapSearch.cs
@@ -14,25 +14,27 @@ public static class MapSearch
         => AetheryteResults(searchTerms)
             .Concat(PointOfInterestResults(searchTerms))
             .Concat(MapResults(searchTerms))
-            .OrderBy(result => result.Label)
+            .OrderByDescending(result => result.Label)
             .Take(numResults)
             .ToList();
 
     private static IEnumerable<ISearchResult> AetheryteResults(string searchTerms)
         => LuminaCache<MapMarker>.Instance
-            .Where(marker => marker is { DataType: 3, PlaceNameSubtext.Value.Name.RawString: not "" })
-            .Where(marker => marker.PlaceNameSubtext.Value!.Name.RawString.ToLowerInvariant().Contains(searchTerms.ToLowerInvariant()))
+            .Where(marker => marker is { DataType: 3 })
+            .Where(marker => LuminaCache<Aetheryte>.Instance.GetRow(marker.DataKey) is { PlaceName.Value.Name.RawString: var aetheryteName } && 
+                             !aetheryteName.IsNullOrEmpty() && 
+                             aetheryteName.ToLowerInvariant().Contains(searchTerms.ToLowerInvariant()) )
             .Select(marker => new AetheryteSearchResult(marker.RowId, marker.SubRowId))
-            .GroupBy(result => result.MapId)
+            .GroupBy(result => result)
             .Select(group => group.First());
 
     private static IEnumerable<ISearchResult> PointOfInterestResults(string searchTerms)
         => LuminaCache<MapMarker>.Instance
             .Where(marker => marker is { DataType: 0, Icon: 60442, PlaceNameSubtext.Value.Name.RawString: not "" })
-            .Where(marker => LuminaCache<Map>.Instance.FirstOrDefault(map => map.MapMarkerRange == marker.RowId) is not null)
+            .Where(marker => LuminaCache<Map>.Instance.FirstOrDefault(map => map.MapMarkerRange == marker.RowId) is { PriorityCategoryUI: not 0, PriorityUI: not 0 })
             .Where(marker => marker.PlaceNameSubtext.Value!.Name.RawString.ToLowerInvariant().Contains(searchTerms.ToLowerInvariant()))
             .Select(group => new PointOfInterestSearchResult(group.RowId, group.SubRowId))            
-            .GroupBy(result => result.MapId)
+            .GroupBy(result => result)
             .Select(group => group.First());
     
     private static IEnumerable<ISearchResult> MapResults(string searchTerms)
@@ -40,6 +42,6 @@ public static class MapSearch
             .Where(map => map is { MapIndex: 0 or 1, Hierarchy: 1, PlaceName.Value.RowId: not 0, IsEvent: false, PriorityUI: not 0 })
             .Where(map => map.PlaceName.Value!.Name.ToDalamudString().TextValue.ToLowerInvariant().Contains(searchTerms.ToLowerInvariant()))
             .Select(map => new MapSearchResult(map.RowId))
-            .GroupBy(result => result.MapId)
+            .GroupBy(result => result)
             .Select(group => group.First());
 }
