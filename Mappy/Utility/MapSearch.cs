9c6766f527000d803f74bd1c1d4c95ac7ed6de23
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-07-15 00:46:31 -0700

    Reduce search restrictiveness

--- Mappy/Utility/MapSearch.cs
+++ Mappy/Utility/MapSearch.cs
@@ -14,34 +14,27 @@ public static class MapSearch
         => AetheryteResults(searchTerms)
             .Concat(PointOfInterestResults(searchTerms))
             .Concat(MapResults(searchTerms))
-            .OrderByDescending(result => result.Label)
+            .OrderBy(result => result.Label)
             .Take(numResults)
             .ToList();
 
     private static IEnumerable<ISearchResult> AetheryteResults(string searchTerms)
         => LuminaCache<MapMarker>.Instance
             .Where(marker => marker is { DataType: 3 })
-            .Where(marker => LuminaCache<Aetheryte>.Instance.GetRow(marker.DataKey) is { PlaceName.Value.Name.RawString: var aetheryteName } && 
-                             !aetheryteName.IsNullOrEmpty() && 
-                             aetheryteName.ToLowerInvariant().Contains(searchTerms.ToLowerInvariant()) )
-            .Select(marker => new AetheryteSearchResult(marker.RowId, marker.SubRowId))
-            .GroupBy(result => result)
-            .Select(group => group.First());
+            .Where(marker => LuminaCache<Aetheryte>.Instance.GetRow(marker.DataKey) is { PlaceName.Value.Name.RawString: var aetheryteName } &&
+                             aetheryteName.ToLowerInvariant().Contains(searchTerms.ToLowerInvariant()))
+            .Select(marker => new AetheryteSearchResult(marker.RowId, marker.SubRowId));
 
     private static IEnumerable<ISearchResult> PointOfInterestResults(string searchTerms)
         => LuminaCache<MapMarker>.Instance
             .Where(marker => marker is { DataType: 0, Icon: 60442, PlaceNameSubtext.Value.Name.RawString: not "" })
             .Where(marker => LuminaCache<Map>.Instance.FirstOrDefault(map => map.MapMarkerRange == marker.RowId) is { PriorityCategoryUI: not 0, PriorityUI: not 0 })
             .Where(marker => marker.PlaceNameSubtext.Value!.Name.RawString.ToLowerInvariant().Contains(searchTerms.ToLowerInvariant()))
-            .Select(group => new PointOfInterestSearchResult(group.RowId, group.SubRowId))            
-            .GroupBy(result => result)
-            .Select(group => group.First());
-    
+            .Select(group => new PointOfInterestSearchResult(group.RowId, group.SubRowId));
+
     private static IEnumerable<ISearchResult> MapResults(string searchTerms)
         => LuminaCache<Map>.Instance
-            .Where(map => map is { MapIndex: 0 or 1, Hierarchy: 1, PlaceName.Value.RowId: not 0, IsEvent: false, PriorityUI: not 0 })
+            .Where(map => map is { Hierarchy: 1, PlaceName.Value.RowId: not 0, IsEvent: false })
             .Where(map => map.PlaceName.Value!.Name.ToDalamudString().TextValue.ToLowerInvariant().Contains(searchTerms.ToLowerInvariant()))
-            .Select(map => new MapSearchResult(map.RowId))
-            .GroupBy(result => result)
-            .Select(group => group.First());
+            .Select(map => new MapSearchResult(map.RowId));
 }
