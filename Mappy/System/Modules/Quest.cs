58036ab65aa02d96e7b1b1715e3a3e37b886dc02
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-07-15 13:08:56 -0700

    Restore questmarkers

--- Mappy/System/Modules/Quest.cs
+++ Mappy/System/Modules/Quest.cs
@@ -73,29 +73,31 @@ public unsafe class Quest : ModuleBase
 
         DebugWindow.Print($"MapDataAddress: {new nint(mapData):X8}");
         
-        // foreach (var quest in mapData->QuestDataSpan)
-        // {
-        //     foreach (var questInfo in quest.MarkerData.Span)
-        //     {
-        //         if (LuminaCache<Level>.Instance.GetRow(questInfo.LevelId) is not { Map.Row: var levelMap }) continue;
-        //         if (levelMap != map.RowId) continue;
-        //         
-        //         DrawRegularObjective(questInfo, quest.Label.ToString(), viewport, map);
-        //     }
-        // }
-
-        foreach (var quest in mapData->QuestMarkerData.GetAllMarkers())
+        foreach (var quest in mapData->QuestDataSpan)
         {
-            foreach (var marker in quest.MarkerData.Span)
+            foreach (var questInfo in quest.MarkerData.Span)
             {
-                DebugWindow.Print($"QuestMarker: {marker.ObjectiveId} : {marker.TooltipString->ToString()}");
-                
-                if (LuminaCache<Level>.Instance.GetRow(marker.LevelId) is not { Map.Row: var levelMap }) continue;
+                DebugWindow.Print($"QuestMarker: {questInfo.ObjectiveId} : {questInfo.TooltipString->ToString()}");
+
+                if (LuminaCache<Level>.Instance.GetRow(questInfo.LevelId) is not { Map.Row: var levelMap }) continue;
                 if (levelMap != map.RowId) continue;
                 
-                DrawRegularObjective(marker, quest.Label.ToString(), viewport, map);
+                DrawRegularObjective(questInfo, quest.Label.ToString(), viewport, map);
             }
         }
+
+        // foreach (var quest in mapData->QuestMarkerData.GetAllMarkers())
+        // {
+        //     foreach (var marker in quest.MarkerData.Span)
+        //     {
+        //         DebugWindow.Print($"QuestMarker: {marker.ObjectiveId} : {marker.TooltipString->ToString()}");
+        //         
+        //         if (LuminaCache<Level>.Instance.GetRow(marker.LevelId) is not { Map.Row: var levelMap }) continue;
+        //         if (levelMap != map.RowId) continue;
+        //         
+        //         DrawRegularObjective(marker, quest.Label.ToString(), viewport, map);
+        //     }
+        // }
     }
     
     private void DrawUnacceptedQuests(Viewport viewport, Map map)
