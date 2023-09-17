493661e5e61326badb22fcb035fd47b711c5bdbf
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-09-17 09:24:36 -0700

    Reorder ZoomOptions

--- Mappy/Models/Configuration/IZoomConfig.cs
+++ Mappy/Models/Configuration/IZoomConfig.cs
@@ -5,12 +5,12 @@ namespace Mappy.Models;
 [Category("ZoomOptions", 3)]
 public interface IZoomConfig
 {
-    [BoolConfig("FocusObjective", "FocusObjectiveHelp")]
-    public bool FocusObjective { get; set; }
-
     [FloatConfig("ZoomSpeed", 0.05f, 0.40f)]
     public float ZoomSpeed { get; set; }
 
     [BoolConfig("ZoomOnFlag", "ZoomOnFlagHelp")]
     public bool ZoomInOnFlag { get; set; }
+    
+    [BoolConfig("FocusObjective", "FocusObjectiveHelp")]
+    public bool FocusObjective { get; set; }
 }
