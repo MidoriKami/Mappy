96abef5413228d0c09c1da9307d1e839fde0249f
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-09-25 22:35:15 -0700

    Use better listclipper

--- Mappy/Views/General/IconDisableView.cs
+++ Mappy/Views/General/IconDisableView.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using Dalamud.Interface;
 using Dalamud.Interface.Utility;
 using ImGuiNET;
-using KamiLib.Windows;
 using Mappy.System;
 using Mappy.System.Localization;
 
@@ -12,9 +12,6 @@ namespace Mappy.Views.General;
 
 public class IconDisableView
 {
-    private int currentIndex;
-    private int itemsPerLine;
-    
     public void Draw()
     {
         if (MappySystem.SystemConfig is not { SeenIcons: var seenIcons }) return;
@@ -24,15 +21,20 @@ public class IconDisableView
         
         if (!seenIcons.Any()) ImGui.TextColored(KnownColor.Orange.Vector(), Strings.IconDisableNoIcons);
 
-        itemsPerLine = (int) MathF.Floor(ImGui.GetContentRegionAvail().X / (64.0f * ImGuiHelpers.GlobalScale + ImGui.GetStyle().ItemSpacing.X));
+        var itemWidth = 64.0f * ImGuiHelpers.GlobalScale + ImGui.GetStyle().ItemSpacing.X;
+        var itemHeight = 64.0f * ImGuiHelpers.GlobalScale + ImGui.GetStyle().ItemSpacing.Y;
         
-        currentIndex = 0;
-        foreach (var icon in seenIcons.OrderBy(id => id).Where(id => id is not (>= 60483 and <= 60494) and not 0))
-        {
-            if (currentIndex >= itemsPerLine && currentIndex % itemsPerLine == 0) ImGui.NewLine();
-            
-            DrawIcon(icon);
-        }
+        var itemsPerLine = (int) MathF.Floor(ImGui.GetContentRegionAvail().X / itemWidth);
+        
+        var filteredIcons = seenIcons
+            .OrderBy(id => id)
+            .Where(id => id is not (>= 60483 and <= 60494)) // Remove invisible icons representing area circles
+            .Where(id => id is not 0) // Remove null icons
+            .Where(id => id is not (>= 62620 and <= 62799)) // Remove special area icons, ie "Gold Saucer"
+            .Where(id => id is not (>= 63201 and <= 063899)) // Remove "Region" icons
+            .ToList();
+        
+        ClippedDraw(filteredIcons, DrawIcon, itemsPerLine, itemHeight);
     }
 
     private void DrawIcon(uint iconId)
@@ -49,8 +51,6 @@ public class IconDisableView
         
         ImGui.Image(iconTexture.ImGuiHandle, size);
         ImGui.GetWindowDrawList().AddRect(start, stop, color, 5.0f, ImDrawFlags.None, 3.0f);
-        ImGui.SameLine();
-        currentIndex++;
 
         if (ImGui.IsItemClicked())
         {
@@ -58,5 +58,54 @@ public class IconDisableView
             else disallowedIcons.Add(iconId);
             MappyPlugin.System.SaveConfig();
         }
+        
+        #if DEBUG
+        if (ImGui.IsItemHovered())
+        {
+            ImGui.SetTooltip($"{iconId}");
+        }
+        #endif
+    }
+    
+    public static void ClippedDraw<T>(IReadOnlyList<T> data, Action<T> draw, int itemsPerLine, float lineHeight)
+    {
+        ImGuiListClipperPtr clipper;
+        unsafe
+        {
+            clipper = new ImGuiListClipperPtr(ImGuiNative.ImGuiListClipper_ImGuiListClipper());
+        }
+        
+        var maxRows = (int) MathF.Ceiling((float)data.Count / itemsPerLine);
+        
+        clipper.Begin(maxRows, lineHeight);
+        while (clipper.Step())
+        {
+            for (var actualRow = clipper.DisplayStart; actualRow < clipper.DisplayEnd; actualRow++)
+            {
+                if (actualRow >= maxRows)
+                    return;
+
+                if (actualRow < 0)
+                    continue;
+
+                var itemsForRow = data
+                    .Skip(actualRow * itemsPerLine)
+                    .Take(itemsPerLine);
+                
+                var currentIndex = 0;
+                foreach (var item in itemsForRow)
+                {
+                    if (currentIndex++ != 0 && currentIndex < itemsPerLine + 1)
+                    {
+                        ImGui.SameLine();
+                    }
+                    
+                    draw(item);
+                }
+            }
+        }
+
+        clipper.End();
+        clipper.Destroy();
     }
 }
