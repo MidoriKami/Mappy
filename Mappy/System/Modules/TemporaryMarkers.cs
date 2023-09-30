0713a5eff31cff7bf060fb0950ffbd2e08a016e6
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-09-29 23:11:06 -0700

    Animate special marker

--- Mappy/System/Modules/TemporaryMarkers.cs
+++ Mappy/System/Modules/TemporaryMarkers.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Numerics;
 using Dalamud.Interface;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
@@ -33,9 +34,12 @@ public unsafe class TemporaryMarkers : ModuleBase
     public static TemporaryMapMarker? FlagMarker { get; private set; }
     public static TemporaryMapMarker? GatheringMarker { get; private set; }
 
+    private int counter;
+
     protected override void DrawMarkers(Viewport viewport, Map map)
     {
         var config = GetConfig<TemporaryMarkersConfig>();
+        counter++;
 
         if (GatheringMarker is not null && GatheringMarker.MapID == map.RowId)
         {
@@ -46,7 +50,7 @@ public unsafe class TemporaryMarkers : ModuleBase
                 
                 Tooltip = GatheringMarker.TooltipText,
                 
-                Radius = GatheringMarker.Radius,
+                Radius = GatheringMarker.Type is MarkerType.Quest ? GetAnimatedRadius(GatheringMarker.Radius, counter, 10.0f) : GatheringMarker.Radius,
                 RadiusColor = config.CircleColor,
             }, config, viewport, map);
             
@@ -66,14 +70,25 @@ public unsafe class TemporaryMarkers : ModuleBase
             FlagMarker.ShowContextMenu(viewport, map);
         }
     }
+
+    private static float GetAnimatedRadius(float originalRadius, int counter, float minRadius)
+        => MathF.Max(minRadius, originalRadius * Pulse(counter));
     
-    public static void SetFlagMarker(TemporaryMapMarker marker) => FlagMarker = marker;
+    private static float Pulse(float time) 
+        => 0.5f * (1 + MathF.Sin(2.0f * MathF.PI * 0.005f * time));
+
+    public static void SetFlagMarker(TemporaryMapMarker marker) 
+        => FlagMarker = marker;
+
     public static void RemoveFlagMarker()
     {
         AgentMap.Instance()->IsFlagMarkerSet = 0;
         FlagMarker = null;
     }
     
-    public static void SetGatheringMarker(TemporaryMapMarker marker) => GatheringMarker = marker;
-    public static void RemoveGatheringMarker() => GatheringMarker = null;
+    public static void SetGatheringMarker(TemporaryMapMarker marker) 
+        => GatheringMarker = marker;
+
+    public static void RemoveGatheringMarker() 
+        => GatheringMarker = null;
 }
