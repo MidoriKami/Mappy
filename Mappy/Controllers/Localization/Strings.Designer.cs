e7be08185583565b8f7d708315b22cf23c52c1e3
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-20 14:19:15 -0700

    Regenerate Localization

--- Mappy/Controllers/Localization/Strings.Designer.cs
+++ Mappy/Controllers/Localization/Strings.Designer.cs
@@ -38,7 +38,7 @@ namespace Mappy.System.Localization {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mappy.System.Localization.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mappy.Controllers.Localization.Strings", typeof(Strings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
