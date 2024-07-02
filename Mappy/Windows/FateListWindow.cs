a9e1fc38348f61806f9510cdc10e23c8c82df456
MidoriKami <mi_kami@outlook.com>
2024-07-02 09:26:34 -0700

    Fix indentation FateListWindow

--- Mappy/Windows/FateListWindow.cs
+++ Mappy/Windows/FateListWindow.cs
@@ -14,54 +14,54 @@ using Mappy.Extensions;
 namespace Mappy.Windows;
 
 public class FateListWindow : Window {
-    private const float ElementHeight = 48.0f;
+	private const float ElementHeight = 48.0f;
     
-    public FateListWindow() : base("Mappy Fate List Window", new Vector2(300.0f, 400.0f)) {
-        AdditionalInfoTooltip = "Shows Fates for the zone you are currently in";
-    }
+	public FateListWindow() : base("Mappy Fate List Window", new Vector2(300.0f, 400.0f)) {
+		AdditionalInfoTooltip = "Shows Fates for the zone you are currently in";
+	}
 
-    protected override unsafe void DrawContents() {
-        if (Service.FateTable.Length > 0) {
-            foreach (var index in Enumerable.Range(0, Service.FateTable.Length)) {
-                var fate = FateManager.Instance()->Fates[index].Value;
+	protected override unsafe void DrawContents() {
+		if (Service.FateTable.Length > 0) {
+			foreach (var index in Enumerable.Range(0, Service.FateTable.Length)) {
+				var fate = FateManager.Instance()->Fates[index].Value;
                 
-                var cursorStart = ImGui.GetCursorScreenPos();
-                if (ImGui.Selectable($"##{fate->FateId}_Selectable", false, ImGuiSelectableFlags.None, new Vector2(ImGui.GetContentRegionAvail().X, ElementHeight * ImGuiHelpers.GlobalScale))) {
-                    System.IntegrationsController.OpenOccupiedMap();
-                    System.SystemConfig.FollowPlayer = false;
-                    System.MapRenderer.DrawOffset = -new Vector2(fate->Location.X, fate->Location.Z);
-                }
+				var cursorStart = ImGui.GetCursorScreenPos();
+				if (ImGui.Selectable($"##{fate->FateId}_Selectable", false, ImGuiSelectableFlags.None, new Vector2(ImGui.GetContentRegionAvail().X, ElementHeight * ImGuiHelpers.GlobalScale))) {
+					System.IntegrationsController.OpenOccupiedMap();
+					System.SystemConfig.FollowPlayer = false;
+					System.MapRenderer.DrawOffset = -new Vector2(fate->Location.X, fate->Location.Z);
+				}
 
-                ImGui.SetCursorScreenPos(cursorStart);
-                    using (ImRaii.Child($"image_child_{fate->FateId}", new Vector2(ElementHeight, ElementHeight), false, ImGuiWindowFlags.NoInputs)) {
-                        ImGui.Image(Service.TextureProvider.GetFromGameIcon(fate->IconId).GetWrapOrEmpty().ImGuiHandle, ImGuiHelpers.ScaledVector2(ElementHeight, ElementHeight));
-                    }
+				ImGui.SetCursorScreenPos(cursorStart);
+				using (ImRaii.Child($"image_child_{fate->FateId}", new Vector2(ElementHeight, ElementHeight), false, ImGuiWindowFlags.NoInputs)) {
+					ImGui.Image(Service.TextureProvider.GetFromGameIcon(fate->IconId).GetWrapOrEmpty().ImGuiHandle, ImGuiHelpers.ScaledVector2(ElementHeight, ElementHeight));
+				}
                     
-                    ImGui.SameLine();
+				ImGui.SameLine();
                     
-                    using (ImRaii.Child($"text_child_{fate->FateId}", new Vector2(ImGui.GetContentRegionAvail().X, ElementHeight), false, ImGuiWindowFlags.NoInputs)) {
-                        ImGui.TextColored(FateContextExtensions.GetColor(fate, 1.0f), $"Lv. {fate->Level} {fate->Name}");
-                        ImGui.TextUnformatted($"Progress: {fate->Progress}%");
+				using (ImRaii.Child($"text_child_{fate->FateId}", new Vector2(ImGui.GetContentRegionAvail().X, ElementHeight), false, ImGuiWindowFlags.NoInputs)) {
+					ImGui.TextColored(FateContextExtensions.GetColor(fate, 1.0f), $"Lv. {fate->Level} {fate->Name}");
+					ImGui.TextUnformatted($"Progress: {fate->Progress}%");
 
-                        var timeRemaining = FateContextExtensions.GetTimeRemaining(fate);
-                        if (timeRemaining != TimeSpan.Zero) {
-                            var timeString = $"{SeIconChar.Clock.ToIconString()} {FateContextExtensions.GetTimeRemaining(fate):mm\\:ss}";
-                            ImGui.SameLine(ImGui.GetContentRegionMax().X - ImGui.CalcTextSize(timeString).X);
-                            ImGui.Text(timeString);
-                        }
-                    }
-            }
-        }
-        else {
-            const string text = "No FATE's available";
-            var textSize = ImGui.CalcTextSize(text);
-            ImGui.SetCursorPosX(ImGui.GetContentRegionAvail().X / 2.0f - textSize.X / 2.0f);
-            ImGui.SetCursorPosY(ImGui.GetContentRegionAvail().Y / 2.0f - textSize.Y / 2.0f);
-            ImGui.TextColored(KnownColor.Orange.Vector(), text);
-        }
-    }
+					var timeRemaining = FateContextExtensions.GetTimeRemaining(fate);
+					if (timeRemaining != TimeSpan.Zero) {
+						var timeString = $"{SeIconChar.Clock.ToIconString()} {FateContextExtensions.GetTimeRemaining(fate):mm\\:ss}";
+						ImGui.SameLine(ImGui.GetContentRegionMax().X - ImGui.CalcTextSize(timeString).X);
+						ImGui.Text(timeString);
+					}
+				}
+			}
+		}
+		else {
+			const string text = "No FATE's available";
+			var textSize = ImGui.CalcTextSize(text);
+			ImGui.SetCursorPosX(ImGui.GetContentRegionAvail().X / 2.0f - textSize.X / 2.0f);
+			ImGui.SetCursorPosY(ImGui.GetContentRegionAvail().Y / 2.0f - textSize.Y / 2.0f);
+			ImGui.TextColored(KnownColor.Orange.Vector(), text);
+		}
+	}
     
-    public override void OnClose() {
-        System.WindowManager.RemoveWindow(this);
-    }
+	public override void OnClose() {
+		System.WindowManager.RemoveWindow(this);
+	}
 }
