f207cd79f443b8f54e966ab007851643c3950d39
MidoriKami <mi_kami@outlook.com>
2024-07-07 13:50:22 -0700

    Fix waymark offset position
    closes #84

--- Mappy/MapRenderer/MapRenderer.Waymark.cs
+++ Mappy/MapRenderer/MapRenderer.Waymark.cs
@@ -2,25 +2,32 @@
 using System.Linq;
 using System.Numerics;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using Mappy.Classes;
-using FieldMarker = Lumina.Excel.GeneratedSheets.FieldMarker;
 using MarkerInfo = Mappy.Classes.MarkerInfo;
+using LuminaFieldMarker = Lumina.Excel.GeneratedSheets.FieldMarker;
 
 namespace Mappy.MapRenderer;
 
 public partial class MapRenderer {
-    private readonly List<FieldMarker> fieldMarkers = Service.DataManager.GetExcelSheet<FieldMarker>()!.Where(marker => marker.MapIcon is not 0).ToList();
+    private readonly List<LuminaFieldMarker> fieldMarkers = Service.DataManager.GetExcelSheet<LuminaFieldMarker>()!.Where(marker => marker.MapIcon is not 0).ToList();
 
     private unsafe void DrawFieldMarkers() {
+        if (AgentMap.Instance()->CurrentMapId != AgentMap.Instance()->SelectedMapId) return;
         var markerSpan = MarkingController.Instance()->FieldMarkers;
         
         foreach (var index in Enumerable.Range(0, 8)) {
             if (markerSpan[index] is { Active: true } marker) {
                 
+                var markerPosition = 
+                    new Vector2(marker.X, marker.Z) / 1000.0f * DrawHelpers.GetMapScaleFactor() 
+                    - DrawHelpers.GetMapOffsetVector() 
+                    + DrawHelpers.GetMapCenterOffsetVector();
+                
                 DrawHelpers.DrawMapMarker(new MarkerInfo {
                     Offset = DrawPosition,
                     Scale = Scale,
-                    Position = (new Vector2(marker.X, marker.Z) / 1000.0f + new Vector2(1024.0f, 1024.0f)) * Scale,
+                    Position = markerPosition * Scale,
                     IconId = fieldMarkers[index].MapIcon,
                 });
             }
