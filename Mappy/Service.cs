e5882852e4b10fd433a82d38eca4d4eeed2f8e58
MidoriKami <mi_kami@outlook.com>
2024-07-23 20:25:45 -0700

    Remove sealed

--- Mappy/Service.cs
+++ Mappy/Service.cs
@@ -5,7 +5,7 @@ using Dalamud.Plugin.Services;
 namespace Mappy;
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-public sealed class Service {
+public class Service {
     [PluginService] public static IDalamudPluginInterface PluginInterface { get; set; }
     [PluginService] public static IClientState ClientState { get; set; }
     [PluginService] public static IFramework Framework { get; set; }
