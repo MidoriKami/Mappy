f56ea22cf0e85a9d9a8d678ed887e9ff7ec420c9
MidoriKami <mi_kami@outlook.com>
2024-08-04 11:17:28 -0700

    cleanup

--- Mappy/MapRenderer/MapRenderer.GameObject.cs
+++ Mappy/MapRenderer/MapRenderer.GameObject.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Numerics;
 using Dalamud.Game.ClientState.Objects.Enums;
 using Dalamud.Game.ClientState.Objects.SubKinds;
