62b0875d10570926f6fc8deb2010415ff60059e5
MidoriKami <mi_kami@outlook.com>
2024-11-17 11:03:19 -0800

    Use HasRow instead of exception checking

--- Mappy/Classes/Caches/TripleTriadCache.cs
+++ Mappy/Classes/Caches/TripleTriadCache.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using KamiLib.Classes;
+﻿using KamiLib.Classes;
 using Lumina.Excel.Sheets;
 
 namespace Mappy.Classes.Caches;
 
 public class TripleTriadCache : Cache<uint, bool> {
-	protected override bool LoadValue(uint key) {
-		try {
-			return Service.DataManager.GetExcelSheet<TripleTriad>().GetRow(key) is { RowId: not 0 };
-		}
-		catch (Exception) {
-			// Probably okay to do this, if we feed a completely invalid valid here, then we don't want to save it to the cache.
-			// This doesn't get called every frame, so it's okay to ignore the errors.
-			return false;
-		}
-	}
+	protected override bool LoadValue(uint key)
+		=> Service.DataManager.GetExcelSheet<TripleTriad>().HasRow(key);
 }
