a8a72d92c92151723da544628c3b6f7b763532f4
MidoriKami <mi_kami@outlook.com>
2025-04-30 19:19:59 -0700

    Refactor Temporary Marker System

--- Mappy/MapRenderer/MapRenderer.Temporary.cs
+++ Mappy/MapRenderer/MapRenderer.Temporary.cs
@@ -1,41 +1,33 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using KamiLib.Extensions;
 using Mappy.Extensions;
 
 namespace Mappy.MapRenderer;
 
 public partial class MapRenderer {
-    // Note, unlike EventMarkers, Temporary Markers seem to layer the icons in reverse.
     private unsafe void DrawTemporaryMarkers() {
         if (AgentMap.Instance()->SelectedMapSub != AgentMap.Instance()->SelectedMapId) return;
         
-        var correctlySizedSpan = AgentMap.Instance()->TempMapMarkers;
+        // Group together icons based on their dataId, this is because square enix shows circles then draws the actual icon overtop
+        var validMarkers = new Span<TempMapMarker>(Unsafe.AsPointer(ref AgentMap.Instance()->TempMapMarkers[0]), AgentMap.Instance()->TempMapMarkers.Length);
+        var iconGroups = validMarkers.ToArray().GroupBy(markers => new Vector2(markers.MapMarker.X, markers.MapMarker.Y));
         
-        for (var index = 0; index < AgentMap.Instance()->TempMapMarkerCount; ++index) {
-            var marker = correctlySizedSpan[index];
-
-            // If there's at least one more marker after this
-            if (index < AgentMap.Instance()->TempMapMarkerCount - 1) {
-                var nextMarker = correctlySizedSpan[index + 1];
-                var currentPosition = new Vector2(marker.MapMarker.X, marker.MapMarker.Y);
-                var nextPosition = new Vector2(nextMarker.MapMarker.X, nextMarker.MapMarker.Y);
-                
-                // Check if the next marker matches our position, and that the next marker has a radius, and we do not.
-                if (currentPosition == nextPosition && marker.MapMarker.Scale <= 1.0f && nextMarker.MapMarker.Scale > 1.0f) {
-                    
-                    // Set the next marker's radius (writing to a copy of the marker, not mutating the original data)
-                    marker.MapMarker.Scale = nextMarker.MapMarker.Scale;
-                    
-                    // Skip drawing the circle marker, and1584. instead just draw the icon marker, with a correct radius.
-                    marker.Draw(DrawPosition, Scale);
-                    
-                    // Now that we have drawn out of order, we need to skip 2 markers.
-                    index++; // Skip the next Marker
-                    continue; // Skip this marker
-                }
-            }
+        foreach (var group in iconGroups) {
+            // Make a copy of the first marker in the set, we will be mutating this copy.
+            var markerCopy = group.First();
+        
+            // Get the actual iconId we want, typically the icon for the marker, not the circle
+            var correctIconId = group.FirstOrNull(marker => marker.MapMarker.IconId is not 60493);
+            markerCopy.MapMarker.IconId = correctIconId?.MapMarker.IconId ?? markerCopy.MapMarker.IconId;
             
-            marker.Draw(DrawPosition, Scale);
+            // Get the actual radius value for this marker, typically the circle icon will have this value.
+            markerCopy.MapMarker.Scale = group.Max(marker => marker.MapMarker.Scale);
+        
+            markerCopy.Draw(DrawPosition, Scale);
         }
     }
 }
