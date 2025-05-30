90c0d5d26caad5e07da3efba6de55606b7c4d48a
MidoriKami <mi_kami@outlook.com>
2025-05-29 23:39:10 -0700

    Cache Safety

--- Mappy/Classes/Caches/TooltipCache.cs
+++ Mappy/Classes/Caches/TooltipCache.cs
@@ -1,10 +1,16 @@
-﻿using System.Linq;
-using KamiLib.Classes;
+﻿using KamiLib.Classes;
 using Lumina.Excel.Sheets;
+using Lumina.Extensions;
 
 namespace Mappy.Classes.Caches;
 
 public class TooltipCache : Cache<uint, string> {
-    protected override string LoadValue(uint key) 
-        => Service.DataManager.GetExcelSheet<MapSymbol>().FirstOrDefault(marker => marker.Icon == key).PlaceName.Value.Name.ExtractText();
+    protected override string LoadValue(uint key) {
+	    var mapMarker = Service.DataManager.GetExcelSheet<MapSymbol>().FirstOrNull(marker => marker.Icon == key);
+
+	    if (mapMarker is null) return string.Empty;
+	    if (!mapMarker.Value.PlaceName.IsValid) return string.Empty;
+
+	    return mapMarker.Value.PlaceName.Value.Name.ExtractText();
+    }
 }
