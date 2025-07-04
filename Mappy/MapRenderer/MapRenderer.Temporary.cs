35dd7ddb63ee8d6d7445a7d89de5ff83d2a8794d
MidoriKami <mi_kami@outlook.com>
2025-07-04 15:29:45 -0700

    Remove special wks handling, not sure what this breaks... but I'll probably be warned eventually.

--- Mappy/MapRenderer/MapRenderer.Temporary.cs
+++ Mappy/MapRenderer/MapRenderer.Temporary.cs
@@ -4,7 +4,6 @@ using System.Numerics;
 using System.Runtime.CompilerServices;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using KamiLib.Extensions;
-using Mappy.Classes;
 using Mappy.Extensions;
 
 namespace Mappy.MapRenderer;
@@ -22,13 +21,8 @@ public partial class MapRenderer {
             var markerCopy = group.First();
         
             // Get the actual iconId we want, typically the icon for the marker, not the circle
-            var correctIconId = group.FirstOrNull(marker => marker.MapMarker.IconId is not 60493);
+            var correctIconId = group.FirstOrNull(marker => marker.MapMarker.IconId is not (60493 or 0));
             markerCopy.MapMarker.IconId = correctIconId?.MapMarker.IconId ?? markerCopy.MapMarker.IconId;
-
-            // Special handling for WKS Markers
-            if (group.Any(marker => marker.Type is 6)) {
-                markerCopy.MapMarker.IconId = DrawHelpers.QuestionMarkIcon;
-            }
             
             // Get the actual radius value for this marker, typically the circle icon will have this value.
             markerCopy.MapMarker.Scale = group.Max(marker => marker.MapMarker.Scale);
